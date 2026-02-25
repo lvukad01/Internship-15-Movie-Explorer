26d0c3f97b8012976e7e8bdbc754e405381e9c62
lvukad01 <lana.vukadin1234@gmail.com>
2026-02-25 16:58:12 +0100

    Create movies page

---
+++ src/data/movies.js
@@ -0,0 +1,45 @@
+const movies=[
+    {
+        id:1,
+        title:"Avatar",
+        year:2009,
+        rating:7.9
+    },
+
+    {
+        id:2,
+        title:"Black Swan",
+        year:2010,
+        rating:8
+    },
+
+    {
+        id:3,
+        title:"Avatar: The Way of Water",
+        year:2022,
+        rating:7.5
+    },
+
+    {
+        id:4,
+        title:"Avatar: Fire and Ash",
+        year:2025,
+        rating:7.4
+    },
+
+    {
+        id:5,
+        title:"Gone girl",
+        year:2014,
+        rating:8.1
+    },
+
+    {
+        id:6,
+        title:"Girl, interrupted",
+        year:1999,
+        rating:7.3
+    }
+]
+
+export default movies;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,54 @@
+import {useEffect, useState} from 'react'
+import moviesData from '../data/movies'
+
+
 export default function Movies(){
 
+  
+  const[search, setSearch]=useState("")
+  const [movies,setMovies]=useState([])
+  const [loading, setLoading]=useState(true)
+  const [error, setError]=useState("")
+
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+        try{
+            setMovies(moviesData)
+            setLoading(false)
+        }catch(err){
+            setError("Something went wrong")
+            setLoading(false)
+        }
+    },1000)
+    return()=>clearTimeout(timer)
+  },[])
+
+  const filteredMovies=movies.filter(movie=>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  )
+
+  if(loading)
+    return <p>Loading movies...</p>
+
+  if(error)
+    return <p>{error}</p>
+
+  return  (
+    <div className='Movies-wrapper'>
+        <input className='searchbar' type="text"
+        placeholder="Search movies..."
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}/>
+        <div className='Movies'>
+         {filteredMovies.length==0 && search!==" " && (
+            <p>No movies found</p>
+         )}
+
+         {filteredMovies.map((movie)=>(
+            <p key={movie.id}>{movie.title} ({movie.year})</p>
+         )
+        )}
+        </div>
+    </div>
+  ) 
 }
