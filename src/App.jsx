9e6fd4debcf3d93caf9631bcc58f0eb5c23d7ecb
lvukad01 <lana.vukadin1234@gmail.com>
2026-02-25 14:53:41 +0100

    Create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Home from './pages/Home.jsx'
+import Favourites from './pages/Favourites.jsx'
+import Movies from './pages/Movies.jsx'
+import MovieDetail from './pages/MovieDetail.jsx'
+import NotFound from './pages/NotFound.jsx'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/movies" element={<Movies/>}/>
+      <Route path="/movies/:id" element={<MovieDetail/>}/>
+      <Route path="/favourites" element={<Favourites/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <div className='title'>
+        <h1>Movie Explorer</h1>
+        <p>Pregledaj filmove i dodaj favorite</p>
+      </div>
+      <div className='links'>
+        <Link to="/movies">Movies</Link>
+        <Link to="/favourites"> Favourites</Link>
+      </div>
+    </div>
+    
+  )
+}
