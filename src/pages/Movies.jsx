3374bde87e1a431e0a3644c81175453aad0ab8fe
lvukad01 <lana.vukadin1234@gmail.com>
2026-03-01 15:47:22 +0100

    UseRef for searchbar focus

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef,useMemo} from 'react'
 import { useNavigate } from 'react-router-dom'
 import moviesData from '../data/movies'
 
@@ -10,9 +10,28 @@ export default function Movies({favorites}){
   const [loading, setLoading]=useState(true)
   const [error, setError]=useState("")
   const [sortBy, setSortBy]=useState("title")
+
+  const searchRef=useRef(null)
+
   const navigate = useNavigate();
 
-  useEffect(()=>{
+    const filteredAndSorted = useMemo(()=>{
+        const filtered = movies.filter(movie =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+      );
+      const sorted=[...filtered]
+
+        if (sortBy === "title") {
+        sorted.sort((a, b) => a.title.localeCompare(b.title));
+        } else if (sortBy === "year") {
+        sorted.sort((a, b) => b.year - a.year);
+        } else if (sortBy === "rating") {
+        sorted.sort((a, b) => b.rating - a.rating);
+    }
+    return sorted
+    },[movies,search,sortBy])
+
+        useEffect(()=>{
     const timer=setTimeout(()=>{
         try{
             setMovies(moviesData)
@@ -24,33 +43,26 @@ export default function Movies({favorites}){
     },1000)
     return()=>clearTimeout(timer)
   },[])
+  useEffect(()=>{
+    searchRef.current?.focus()
 
-
-  const filtered = movies.filter(movie =>
-  movie.title.toLowerCase().includes(search.toLowerCase())
-);
-    const filteredAndSorted = [...filtered];
-
-    if (sortBy === "title") {
-        filteredAndSorted.sort((a, b) => a.title.localeCompare(b.title));
-        } else if (sortBy === "year") {
-        filteredAndSorted.sort((a, b) => b.year - a.year);
-        } else if (sortBy === "rating") {
-        filteredAndSorted.sort((a, b) => b.rating - a.rating);
-    }
-
+  })
   if(loading)
     return <p>Loading movies...</p>
 
   if(error)
     return <p>{error}</p>
 
+
+
   return  (
     <div className='Movies-wrapper'>
-        <input className='searchbar' type="text"
-        placeholder="Search movies..."
-        value={search}
-        onChange={(e)=>setSearch(e.target.value)}/>
+        <input
+          ref={searchRef}
+          type="text"
+          placeholder="Search movies..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}/>
         <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
             <option value="title">Title</option>
             <option value="year">Year</option>
